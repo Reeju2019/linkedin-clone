94ea5aac30c857865259d4910d8dd9dce495aecd
Reeju Bhattacherji <reejubhattacherji@gmail.com>
2021-08-28 11:58:28 +0530

    Adding the signin component

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,8 +11,12 @@ const Login = (props) => {
                     <Join>
                         Join now
                     </Join>
+                    <SignIn>
+                        Sign In
+                    </SignIn>
                 </div>
             </Nav>
+            <Section>Section Component</Section>
         </Container>  
     );
 };
@@ -33,24 +37,52 @@ flex-wrap: nowrap;
 & > a {
     width: 135px;
     height: 34px;
+    cursor: pointer;
     @media (max-width: 768px) {
         padding: 0 5px;
     }
 }
 `;
 
-const Join = styled.div`
+const Join = styled.a`
 font-size: 16px;
 padding: 10px 12px;
 text-decoration: none;
 border-radius: 4px;
 color: rgba(0, 0, 0, 0.6);
 margin-right: 12px;
+cursor: pointer;
 &:hover{
     background-color: rgba(0, 0, 0, 0.08);
     color: rgba(0, 0, 0, 0.9);
     text-decoration: none;
 }
+`;
+
+const SignIn = styled.a`
+box-shadow: inset 0 0 0 1px #0a66c2;
+color: #0a66c2;
+border-radius: 24px;
+transition-duration: 168ms;
+font-size: 16px;
+font-weight: 600;
+line-height: 40px;
+padding: 10px 24px;
+text-align: center;
+cursor: pointer;
+background-color: rgba(0, 0, 0, 0);
+&:hover {
+    background-color: rgba(112, 181, 249, 0.15);
+    color: #0a66c2;
+    text-decoration: none;
+}
+`;
+
+const Section = styled.section`
+display: flex;
+align-content: flex-start;
+min-height: 700px;
+padding-bottom: 138px;
 `;
 
 export default Login;
