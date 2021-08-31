d3028684bffd72791de40a8fc71a503c8cd6078c
Reeju Bhattacherji <reejubhattacherji@gmail.com>
2021-09-01 02:55:08 +0530

    Adding the leftside of home component

---
+++ src/components/Rightside.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+function Rightside() {
+  return (
+    <Container>
+      Rightside
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  grid-area: Sightside;
+`;
+
+export default Rightside;
